aef86a623f84266ccee219238ede66c3fbee0b87
comsize-yuta-azuma <yuta.azuma@comsize.com>
2024-10-10 17:52:46 +0900

    TaskListDAO内SELECT文に誤りがあったため修正しています。

--- src/main/java/model/dao/TaskListDAO.java
+++ src/main/java/model/dao/TaskListDAO.java
@@ -26,8 +26,8 @@ public class TaskListDAO {
 		sb.append(" t1.task_name,");
 		sb.append(" t2.category_name,");
 		sb.append(" t1.limit_date,");
-		sb.append(" t3.user_id,");
-		sb.append(" t4.status_code,");
+		sb.append(" t3.user_name,");
+		sb.append(" t4.status_name,");
 		sb.append(" t1.memo,");
 		sb.append(" t1.create_datetime,");
 		sb.append(" t1.update_datetime ");
@@ -36,7 +36,7 @@ public class TaskListDAO {
 		sb.append("JOIN");
 		sb.append(" m_category t2 ");
 		sb.append("ON");
-		sb.append(" t1.category_id = t2.category_id, ");
+		sb.append(" t1.category_id = t2.category_id ");
 		sb.append("JOIN");
 		sb.append(" m_user t3 ");
 		sb.append("ON");
@@ -45,7 +45,7 @@ public class TaskListDAO {
 		sb.append(" m_status t4 ");
 		sb.append("ON");
 		sb.append(" t1.status_code = t4.status_code ");
-		sb.append("ORDER BY t1.task_id");
+		sb.append("ORDER BY t1.task_id ");
 		//WHEREで条件指定の必要性が今後発生する？
 		//sb.append("WHERE ");
 		String sql = sb.toString();
