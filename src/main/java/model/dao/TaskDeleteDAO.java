506d70df8b11c4802581db986e1022a057931246
comsize-TAGUCHI <mikuto.taguchi@comsize.com>
2024-10-11 11:01:49 +0900

    リスト削除機能(途中)

--- src/main/java/model/dao/TaskDeleteDAO.java
+++ src/main/java/model/dao/TaskDeleteDAO.java
@@ -1,5 +1,30 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.entity.TaskListBean;
+
 public class TaskDeleteDAO {
 
+	public int deleteTask(TaskListBean task) throws ClassNotFoundException, SQLException {
+
+		int deleteNumber = 0;
+
+		String sql = "DELETE FROM t_task WHERE task_name = ?,category_id = ?, limit_date = ?,staus_code = ?";
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+			pstmt.setString(1, task.getTaskName());
+			pstmt.setInt(2, task.getCategoryId());
+			pstmt.setDate(3, task.getLimitDate());
+			pstmt.setString(4, task.getStatusCode());
+
+			deleteNumber = pstmt.executeUpdate();
+		}
+		return deleteNumber;
+
+	}
 }
--- src/main/java/servlet/TaskDeleteServlet.java
+++ src/main/java/servlet/TaskDeleteServlet.java
@@ -1,7 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.dao.TaskDeleteDAO;
+import model.entity.TaskListBean;
 import model.entity.UserBean;
 
 /**
@@ -17,19 +21,20 @@ import model.entity.UserBean;
 @WebServlet(name = "/list/delete", urlPatterns = { "/taskdeleteservlet" })
 public class TaskDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TaskDeleteServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TaskDeleteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -37,15 +42,27 @@ public class TaskDeleteServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		//セッション保持
 		HttpSession session = request.getSession();
 		UserBean userInfo = (UserBean) session.getAttribute("userInfo");
-		
+		//削除用DAOの生成
+		TaskDeleteDAO dao = new TaskDeleteDAO();
+		//削除件数
 		int deleteNumber = 0;
 		
-		
-		
-	}
+		String url = "Task-delete.jsp";
 
+		try {
+			//削除処理
+			deleteNumber = dao.deleteTask((TaskListBean) (request.getAttribute("task_name")));
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("deleteNumber", deleteNumber);
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
+	}
 }
