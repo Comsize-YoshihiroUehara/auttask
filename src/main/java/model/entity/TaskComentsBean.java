dac93e8c4edba8a8dc2b184e5ddfbd64eabd3e3d
comsize-TAGUCHI <mikuto.taguchi@comsize.com>
2024-10-21 11:33:42 +0900

    #26　コメント機能のBeanを作成しました

---
+++ src/main/java/model/entity/TaskComentsBean.java
@@ -0,0 +1,45 @@
+package model.entity;
+
+import java.sql.Timestamp;
+
+public class TaskComentsBean {
+	
+	private int comentId;
+	private int taskId;
+	private String userId;
+	private String comment;
+	private  Timestamp updateDateTime;
+	
+	public int getComentId() {
+		return comentId;
+	}
+	public void setComentId(int comentId) {
+		this.comentId = comentId;
+	}
+	public int getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public Timestamp getUpdateDateTime() {
+		return updateDateTime;
+	}
+	public void setUpdateDateTime(Timestamp updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+	
+
+}
