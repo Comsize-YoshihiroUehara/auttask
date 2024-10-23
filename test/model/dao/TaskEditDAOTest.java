b8347f93110aca28967d13108f94f4805b973065
comsize-TAGUCHI <mikuto.taguchi@comsize.com>
2024-10-23 16:44:34 +0900

    #28 カテゴリーリスト取得テスト()・ユーザーリスト取得テスト()・ステータスリスト取得テスト()のテストコードを作成しました

--- test/model/dao/TaskEditDAOTest.java
+++ test/model/dao/TaskEditDAOTest.java
@@ -1,20 +1,27 @@
 package model.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.sql.Date;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.entity.CategoryBean;
+import model.entity.StatusBean;
+import model.entity.UserBean;
 import model.form.TaskEditForm;
 
 public class TaskEditDAOTest {
 	private TaskEditDAO taskEditDAO;
 	private TaskEditForm taskEditForm;
 	private int rowsAffected;
-
+	private List<CategoryBean> categoryList;
+	private List<UserBean> userList;
+	private List<StatusBean> statusList;
 	@BeforeEach
 	void setUp_テスト準備() {
 		taskEditDAO = new TaskEditDAO();
@@ -48,7 +55,7 @@ public class TaskEditDAOTest {
 		assertNotNull(taskEditForm);
 		assertEquals(0, taskEditForm.getTaskId());
 	}
-	
+		
 	@Test
 	void test3_タスク追加成功テスト() {
 		java.sql.Date date = Date.valueOf("2030-01-22");
@@ -71,5 +78,41 @@ public class TaskEditDAOTest {
 		}
 		
 		assertEquals(1, rowsAffected);
+	}
+	
+	@Test
+	public void カテゴリーリスト取得テスト() {
+		
+		try {
+			categoryList = taskEditDAO.selectAllCategory();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		assertNotNull(categoryList);
+	}
+	
+	@Test
+	public void ユーザーリスト取得テスト() {
+		
+		try {
+			userList = taskEditDAO.selectAllUser();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		assertNotNull(userList);
+	}
+	
+	@Test
+	public void ステータスリスト取得テスト() {
+		
+		try {
+			statusList = taskEditDAO.selectAllStatus();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		assertNotNull(statusList);
 	}
 }
