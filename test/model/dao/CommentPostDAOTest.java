97b204238e23a05c4b4e25b642cad5089cf884e1
comsize-TAGUCHI <mikuto.taguchi@comsize.com>
2024-10-23 16:21:54 +0900

    #28 コメント取得テスト()のモック化を行いました

--- test/model/dao/CommentPostDAOTest.java
+++ test/model/dao/CommentPostDAOTest.java
@@ -1,39 +1,79 @@
 package model.dao;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import model.entity.TaskCommentsBean;
 
 class CommentPostDAOTest {
-	private CommentPostDAO dao;
-	private TaskCommentsBean bean;
+
+	@InjectMocks
+	CommentPostDAO commentpostdao;
+
+	@Mock
+	Connection con;
+
+	@Mock
+	PreparedStatement pstmt;
+
+	@Mock
+	ResultSet rs;
+
+	private AutoCloseable closeable;
+
+	@BeforeEach
+	void setUp() throws ClassNotFoundException, SQLException {
+		Mockito.clearAllCaches(); // キャッシュをクリア
+		closeable = MockitoAnnotations.openMocks(this);
+		Mockito.mockStatic(ConnectionManager.class);
+		when(ConnectionManager.getConnection()).thenReturn(con);
+	}
+
+	private CommentPostDAO commentpostdao1;
+	private TaskCommentsBean taskcommentsbean;
 
 	@BeforeEach
 	public void テスト準備() {
-		dao = new CommentPostDAO();
-		bean = new TaskCommentsBean();
+		commentpostdao = new CommentPostDAO();
+		taskcommentsbean = new TaskCommentsBean();
 	}
 
 	@Test
-	public void コメント取得テスト() {
+	public void コメント取得テスト() throws SQLException {
 		int rowsAffected = 0;
 
-		bean.setTaskId(1);
-		bean.setUserId("コメ");
-		bean.setComment("は");
+		taskcommentsbean.setTaskId(1);
+		taskcommentsbean.setUserId("コメ");
+		taskcommentsbean.setComment("は");
+
+		when(con.prepareStatement(anyString())).thenReturn(pstmt);
+		when(pstmt.executeUpdate()).thenReturn(1);
 
 		try {
-			rowsAffected = dao.insertComment(bean);
+			rowsAffected = commentpostdao.insertComment(taskcommentsbean);
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-		assertEquals(0, rowsAffected);
+		assertEquals(1, rowsAffected);
+
+		verify(pstmt, times(1)).setInt(1, taskcommentsbean.getTaskId());
+		verify(pstmt, times(1)).setString(2, taskcommentsbean.getUserId());
+		verify(pstmt, times(1)).setString(3, taskcommentsbean.getComment());
+		verify(pstmt, times(1)).executeUpdate();
 	}
 
 }
